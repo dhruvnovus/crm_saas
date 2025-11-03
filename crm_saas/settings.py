10a265ba65635d652d112895671ea68e3e53de15
Dhruv Novuscode <dhruv@novuscode.com>
2025-11-03 17:07:37 +0530

    first commit

--- crm_saas/settings.py
+++ crm_saas/settings.py
@@ -102,8 +102,8 @@ DATABASES = {
         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.mysql'),
         'NAME': os.getenv('DB_NAME', 'crm_saas_main'),
         'USER': os.getenv('DB_USER', 'root'),
-        'PASSWORD': os.getenv('DB_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'Novuscode123'),
+        'HOST': os.getenv('DB_HOST', 'all-database.cgn118uneswh.us-east-2.rds.amazonaws.com'),
         'PORT': os.getenv('DB_PORT', '3306'),
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
